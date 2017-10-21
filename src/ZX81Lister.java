d4ff6184113ed0b5a5ff348a81c694d26664c026
Tom White <tom@cloudera.com>
2017-10-21 14:55:00 +0100

    Debugging

--- src/ZX81Lister.java
+++ src/ZX81Lister.java
@@ -12,12 +12,16 @@ import java.util.TreeMap;
  */
 public class ZX81Lister {
     public static void main(String[] args) throws Exception {
-        //File f = new File("/Users/tom/projects-workspace/zx81/white-tape/frogging/frogging-normalized.1.program1.p");
-        File f = new File("/Users/tom/projects-workspace/zx81/randompatterns.p");
+        File f = new File("/Users/tom/projects-workspace/zx81/white-tape/frogging/frogging-normalized.1.program1.p");
+        //File f = new File("/Users/tom/projects-workspace/zx81/randompatterns.p");
         FileInputStream fis = new FileInputStream(f);
         byte[] fileBytes = new byte[fis.available()];
         fis.read(fileBytes);
         fis.close();
+
+        // insert two bits to see if we can get frogging program out...
+        fileBytes = BitUtils.insert(fileBytes, 8 * (ZX81SysVars.PRBUFF - ZX81SysVars.SAVE_START), false);
+        fileBytes = BitUtils.insert(fileBytes, 8 * (ZX81SysVars.MEMBOT - ZX81SysVars.SAVE_START), false);
 
         System.out.println("=== File");
         System.out.println("Length: " + fileBytes.length);
@@ -40,6 +44,10 @@ public class ZX81Lister {
         System.out.println(fileBytes[117]); // first line number is 12, not 10 - perhaps a 0 bit was dropped? try inserting one?
         System.out.println(fileBytes[118]);
         System.out.println(fileBytes[119]);
+        int ln = ((fileBytes[116] & 255) << 8) + (fileBytes[117] & 255);
+        int ll = (fileBytes[118] & 255) + ((fileBytes[119] & 255) << 8);
+        System.out.println("ln: " + ln);
+        System.out.println("ll: " + ll);
         Map<Integer, byte[]> programLines = ZX81Basic.getProgramLines(fileBytes);
         for (Map.Entry<Integer, byte[]> line : programLines.entrySet()) {
             int lineNumber = line.getKey();
